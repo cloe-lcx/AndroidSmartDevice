24db31eb804ca73d35eaad4b680bf3b9215c6229
cloe-lcx <cloe.lacroix13@gmail.com>
2025-03-24 16:56:32 +0100

    appareils qui s'affichent

--- app/src/main/java/fr/isen/cloelacroix/androidsmartdevice/ScanActivity.kt
+++ app/src/main/java/fr/isen/cloelacroix/androidsmartdevice/ScanActivity.kt
@@ -1,9 +1,23 @@
 package fr.isen.cloelacroix.androidsmartdevice
 
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.le.BluetoothLeScanner
+import android.bluetooth.le.ScanCallback
+import android.bluetooth.le.ScanResult
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.LocationManager
+import android.os.Build
 import android.os.Bundle
+import android.os.Handler
+import android.provider.Settings
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,25 +29,59 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Close
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import fr.isen.cloelacroix.androidsmartdevice.ui.theme.AndroidSmartDeviceTheme
 
 class ScanActivity : ComponentActivity() {
     private val scanResults = mutableStateListOf<BleDevice>()
     private var isScanning by mutableStateOf(false)
-    private var progress by mutableStateOf(0f)  // Progression du scan
+    private var progress by mutableStateOf(0f)
+    private val bluetoothAdapter: BluetoothAdapter? by lazy {
+        BluetoothAdapter.getDefaultAdapter()
+    }
+    private val bluetoothLeScanner: BluetoothLeScanner? by lazy {
+        bluetoothAdapter?.bluetoothLeScanner
+    }
+    private val isBluetoothEnabled: Boolean
+        get() = bluetoothAdapter?.isEnabled == true
+
+    private val requestBluetoothPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            if (permissions.all { it.value }) {
+                startScan()
+            } else {
+                Toast.makeText(this, "Permissions refusées", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+    private val enableBluetoothLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (isBluetoothEnabled) {
+                checkLocationAndStartScan()
+            } else {
+                Toast.makeText(this, "Bluetooth non activé", Toast.LENGTH_SHORT).show()
+            }
+        }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             AndroidSmartDeviceTheme {
+                val context = LocalContext.current
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     ScanScreen(
-                        onScanClick = { if (isScanning) stopScan() else startScan() },
+                        onScanClick = {
+                            checkBluetoothAndLocationAndStartScan()
+                        },
+                        onStopScanClick = {
+                            stopScan() // Arrêter le scan manuellement
+                        },
                         scanResults = scanResults,
                         isScanning = isScanning,
                         progress = progress,
@@ -44,42 +92,135 @@ class ScanActivity : ComponentActivity() {
         }
     }
 
+    private fun checkBluetoothAndLocationAndStartScan() {
+        if (isBluetoothEnabled) {
+            checkLocationAndStartScan()
+        } else {
+            Toast.makeText(this, "Veuillez activer le Bluetooth", Toast.LENGTH_SHORT).show()
+            enableBluetooth()
+        }
+    }
+
+    private fun checkLocationAndStartScan() {
+        val locationManager = getSystemService(LOCATION_SERVICE) as LocationManager
+        val isLocationEnabled = locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+
+        if (isLocationEnabled) {
+            checkBluetoothPermissionsAndStartScan()
+        } else {
+            Toast.makeText(this, "Veuillez activer la localisation", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+        }
+    }
+
+    private fun checkBluetoothPermissionsAndStartScan() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val permissions = arrayOf(
+                Manifest.permission.BLUETOOTH_SCAN,
+                Manifest.permission.BLUETOOTH_CONNECT,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            val missingPermissions = permissions.filter {
+                ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+            }.toTypedArray()
+
+            if (missingPermissions.isNotEmpty()) {
+                requestBluetoothPermissionLauncher.launch(missingPermissions)
+            } else {
+                startScan()
+            }
+        } else {
+            val permissions = arrayOf(
+                Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            )
+            val missingPermissions = permissions.filter {
+                ContextCompat.checkSelfPermission(this, it) != PackageManager.PERMISSION_GRANTED
+            }.toTypedArray()
+
+            if (missingPermissions.isNotEmpty()) {
+                requestBluetoothPermissionLauncher.launch(missingPermissions)
+            } else {
+                startScan()
+            }
+        }
+    }
+
+    private fun enableBluetooth() {
+        if (bluetoothAdapter != null) {
+            if (!isBluetoothEnabled) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                enableBluetoothLauncher.launch(enableBtIntent)
+            }
+        }
+    }
+
+    private val scanTimeout: Long = 20000 // Durée maximale du scan en millisecondes (10 secondes)
+    private var scanHandler: Handler? = null
+
     private fun startScan() {
         isScanning = true
         progress = 0f
-        scanResults.clear()  // Effacer les résultats précédents
-        // Ajouter les faux appareils
-        scanResults.addAll(listOf(
-            BleDevice("Device Unknown", "47:28:A3:7C:E2:BE", -84),
-            BleDevice("Labo_IoT", "00:80:E1:26:6F:6E", -61),
-            BleDevice("Device Unknown", "53:C7:E7:5E:D8:2E", -88),
-            BleDevice("Device Unknown", "4A:6C:22:2E:ED:DE", -97),
-            BleDevice("Device Unknown", "55:F7:CF:C6:17:70", -83),
-            BleDevice("Device Unknown", "FF:CC:05:0F:05:00", -86)
-        ))
-        // Simuler le progrès
+        scanResults.clear()
+        bluetoothLeScanner?.startScan(scanCallback)
+
+        // Arrêter le scan après un délai de `scanTimeout`
+        scanHandler = Handler()
+        scanHandler?.postDelayed({
+            stopScan()
+        }, scanTimeout)
+
         simulateProgress()
     }
 
     private fun stopScan() {
         isScanning = false
+        bluetoothLeScanner?.stopScan(scanCallback)
+        scanHandler?.removeCallbacksAndMessages(null)
     }
 
-    // Simuler la progression du scan
     private fun simulateProgress() {
-        // Augmenter la progression toutes les 100ms
-        for (i in 1..100) {
-            android.os.Handler().postDelayed({
-                progress = i / 100f
-                if (i == 100) stopScan()
-            }, (i * 100).toLong())
+        val handler = Handler()
+        for (i in 1..200) {
+            handler.postDelayed({
+                progress = i / 200f
+                if (i == 200) stopScan()
+            }, (i * 200).toLong())
         }
     }
+
+    private val scanCallback = object : ScanCallback() {
+        override fun onScanResult(callbackType: Int, result: ScanResult) {
+            val device = result.device
+            val rssi = result.rssi
+            val name = device.name ?: "Device Unknown"
+
+            // Filtrer les périphériques sans nom
+            if (name != "Device Unknown") {
+                val macAddress = device.address
+
+                // Ajouter seulement si l'appareil n'est pas déjà dans la liste
+                val bleDevice = BleDevice(name, macAddress, rssi)
+                if (scanResults.find { it.macAddress == macAddress } == null) {
+                    scanResults.add(bleDevice)
+                }
+            }
+        }
+
+        override fun onScanFailed(errorCode: Int) {
+            Toast.makeText(this@ScanActivity, "Scan failed: $errorCode", Toast.LENGTH_SHORT).show()
+            stopScan()
+        }
+    }
+
 }
 
 @Composable
 fun ScanScreen(
     onScanClick: () -> Unit,
+    onStopScanClick: () -> Unit,  // Ajouter une fonction pour arrêter le scan manuellement
     scanResults: List<BleDevice>,
     isScanning: Boolean,
     progress: Float,
@@ -90,6 +231,7 @@ fun ScanScreen(
         verticalArrangement = Arrangement.Top,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
+
         // Titre de l'écran
         Text(
             text = "AndroidSmartDevice",
@@ -110,7 +252,8 @@ fun ScanScreen(
             if (isScanning) {
                 Text(text = "Scan BLE en cours", modifier = Modifier.padding(end = 8.dp), fontSize = 22.sp)
 
-                IconButton(onClick = { onScanClick() }) {
+                // Bouton pour arrêter le scan manuellement
+                IconButton(onClick = { onStopScanClick() }) {
                     Icon(imageVector = Icons.Filled.Close, contentDescription = "Stop", tint = Color.Red, modifier = Modifier.size(32.dp))
                 }
             } else {
@@ -156,22 +299,35 @@ fun BleDeviceItem(device: BleDevice) {
         Column(modifier = Modifier.padding(16.dp)) {
             Row(verticalAlignment = Alignment.CenterVertically) {
                 Text(text = "${device.rssi}", fontWeight = FontWeight.Bold, fontSize = 20.sp, color = Color.Blue)
+
                 Spacer(modifier = Modifier.width(8.dp))
-                Column {
-                    Text(text = "Nom: ${device.name}")
-                    Text(text = "Adresse MAC: ${device.macAddress}")
+
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(text = "Device Name: ${device.deviceName}")
+                    Text(text = "MAC Address: ${device.macAddress}")
                 }
             }
         }
     }
 }
 
-data class BleDevice(val name: String, val macAddress: String, val rssi: Int)
+data class BleDevice(val deviceName: String, val macAddress: String, val rssi: Int)
 
 @Preview(showBackground = true)
 @Composable
-fun ScanScreenPreview() {
+fun DefaultPreview() {
     AndroidSmartDeviceTheme {
-        ScanScreen(onScanClick = {}, scanResults = emptyList(), isScanning = false, progress = 0f)
+        ScanScreen(
+            onScanClick = {},
+            scanResults = listOf(
+                BleDevice("Device 1", "00:11:22:33:44:55", -60),
+                BleDevice("Device 2", "00:11:22:33:44:66", -50),
+                BleDevice("Device 3", "00:11:22:33:44:77", -40)
+            ),
+            isScanning = false,
+            progress = 0f,
+            onStopScanClick = TODO(),
+            modifier = TODO()
+        )
     }
 }
